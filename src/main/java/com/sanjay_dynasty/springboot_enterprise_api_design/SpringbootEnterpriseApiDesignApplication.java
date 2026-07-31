911cc0b40b3a1103f3b21488eb6f8547e59d4826
Sanjay Kumar <ersanjay426@gmail.com>
2026-07-31 19:55:25 +0530

    Initial Project setup

---
+++ src/main/java/com/sanjay_dynasty/springboot_enterprise_api_design/SpringbootEnterpriseApiDesignApplication.java
@@ -0,0 +1,14 @@
+package com.sanjay_dynasty.springboot_enterprise_api_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEnterpriseApiDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEnterpriseApiDesignApplication.class, args);
+		System.out.println("Application is running ..... ");
+	}
+
+}
---
+++ src/test/java/com/sanjay_dynasty/springboot_enterprise_api_design/SpringbootEnterpriseApiDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay_dynasty.springboot_enterprise_api_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEnterpriseApiDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
