826499a73244fbbcbca7de4b9ec61823728e0d7e
Sanjay Kumar <ersanjay426@gmail.com>
2026-08-03 07:43:35 +0530

    Phase 4.3 : Complete JWT parsing and validation

--- src/main/java/com/sanjay_dynasty/springboot_enterprise_api_design/security/JwtService.java
+++ src/main/java/com/sanjay_dynasty/springboot_enterprise_api_design/security/JwtService.java
@@ -3,6 +3,7 @@ package com.sanjay_dynasty.springboot_enterprise_api_design.security;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.sanjay_dynasty.springboot_enterprise_api_design.config.JwtProperties;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
@@ -20,19 +22,52 @@ public class JwtService {
 
     private final JwtProperties jwtProperties;
 
-    public String generateToken(String username) {
-
-        SecretKey key = Keys.hmacShaKeyFor(jwtProperties
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtProperties
                 .secret()
                 .getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String username) {
 
         return Jwts.builder()
                 .claims(new HashMap<>())
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + jwtProperties.expiration()))
-                .signWith(key)
+                .signWith(getSigningKey())
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+
+    }
+
+    public Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenExpired(String token) {
+
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, String username) {
+
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
     }
 
 }
